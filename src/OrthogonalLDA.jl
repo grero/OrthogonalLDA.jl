57cc43c0eb8fa74e019aaee010f6a6eee2f691fd
roger <roger.herikstad@gmail.com>
2022-06-28 14:28:32 +0800

    Initial commit

---
+++ src/OrthogonalLDA.jl
@@ -0,0 +1,118 @@
+module OrthogonalLDA
+
+using Manifolds
+using Manopt
+using ManifoldsBase
+using LinearAlgebra
+
+import Manifolds: _vector_transport_to
+
+struct IdentityTransport <: AbstractVectorTransportMethod
+end
+
+Manifolds._vector_transport_to(M::Stiefel, Y, p, X, q, T::IdentityTransport) = (Y .= project(M, q, X))
+
+function orthogonal_lda(Sb::AbstractMatrix{Float64}, Sw::AbstractMatrix{Float64}, r::Int64;debug::Union{Missing, Symbol}=:default, method=:quasi_newton)
+    if !ismissing(debug) && debug == :default
+		debug=[:Iteration, " ", :Cost, "\n", 1, :Stop]
+	end
+	d = size(Sb,1)
+	d == size(Sb,2) == size(Sw,1) == size(Sw,2) || error("Matrices must be of equal size")
+	M = Stiefel(d, r)
+	
+	function F(::Stiefel, W::Array{Float64,2})
+		-tr(W'*Sb*W)/tr(W'*Sw*W)
+	end
+	
+	#G = fill(0.0, d, r)
+	function gradF(M, W::Array{Float64,2})
+		trb = tr(W'*Sb*W)
+		trw = tr(W'*Sw*W)
+		G = -((Sb*W + Sb'*W).*trw - trb.*(Sw*W + Sw'*W))./trw^2
+		return project(M, W, G)
+	end
+	w = random_point(M)
+	f0 = F(M, w)
+	if method == :quasi_newton
+		w = quasi_Newton(
+		    M,
+		    F,
+		    gradF,
+		    w;
+		    memory_size=32,
+			#evaluation=MutatingEvaluation(),
+		    cautious_update=true,
+            vector_transport_method=ProjectionTransport(),
+		    stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-9)),
+		    debug=debug,
+		)
+	elseif method == :gradient_descent
+		w = gradient_descent(
+		    M,
+		    F,
+		    gradF,
+		    w;
+		    stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-8)),
+		    debug=debug,
+		)
+	elseif method == :trust_regions
+		w = trust_regions(M, 
+						  F, 
+						  gradF, 
+						  ApproxHessianFiniteDifference(M, w, gradF),
+						  w;
+						#vector_transport_method=IdentityTransport(),
+		    			#stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-8)),
+		    			debug=debug
+		)
+	else
+		error("Unknown method $(method)")
+	end
+	f1 = F(M, w)
+	w, f0, f1
+end
+
+function orthogonal_lda(Sb1::AbstractMatrix{Float64}, Sb2::AbstractMatrix{Float64},Sw1::AbstractMatrix{Float64}, Sw2::AbstractMatrix{Float64}, r::Int64;debug=:default, method=:quasi_newton)
+    d1 = size(Sb1,1)
+    d2 = size(Sb2,1)
+
+	if !ismissing(debug) && debug == :default
+		debug=[:Iteration, " ", :Cost, "\n", 1, :Stop]
+	end
+
+	function F(::Stiefel, W::Array{Float64,2})
+        W1 = W[1:d1,:]
+        W2 = W[d1+1:d1+d2,:]
+        a = -tr(W1'*Sb1*W1)/tr(W1'*Sw1*W1)
+        b = -tr(W2'*Sb2*W2)/tr(W2'*Sw2*W2)
+        a + b
+    end
+
+	function gradF(M, W::Array{Float64,2})
+        W1 = W[1:d1,:]
+        W2 = W[d1+1:d1+d2,:]
+		trb1 = tr(W1'*Sb1*W1)
+		trb2 = tr(W2'*Sb2*W2)
+		trw1 = tr(W1'*Sw1*W1)
+		trw2 = tr(W2'*Sw2*W2)
+		G1 = -((Sb1*W1 + Sb1'*W1).*trw1 - trb1.*(Sw1*W1 + Sw1'*W1))./trw1^2
+		G2 = -((Sb2*W2 + Sb2'*W2).*trw2 - trb2.*(Sw2*W2 + Sw2'*W2))./trw2^2
+        return project(M, W, cat(G1,G2,dims=1))
+	end
+
+	M = Stiefel(d1+d2, r)
+	w = random_point(M)
+    w = quasi_Newton(
+        M,
+        F,
+        gradF,
+        w;
+        memory_size=32,
+        #evaluation=MutatingEvaluation(),
+        cautious_update=true,
+        vector_transport_method=ProjectionTransport(),
+        stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-9)),
+        debug=debug,
+    )
+end
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,99 @@
+using OrthogonalLDA
+using LinearAlgebra
+using MultivariateStats
+using Statistics
+
+using Test
+
+
+@testset "Basic" begin
+
+
+    d = 4
+    # 1st plane
+    μ1 = fill(0.0, d, 3)
+    μ1[:,1] = [0.0,1.0, 0.0, 0.0]
+    μ1[:,2] = [1.0, 0.0, 0.0, 0.0]
+    μ1[:,3] = [1.0, 1.0, 0.0, 0.0]
+    
+    # 2nd plane
+    μ2  = fill(0.0, d, 3)
+    μ2[:,1] = [0.0, 0.0, 0.0, 1.0]
+    μ2[:,2] = [0.0, 0.0, 1.0, 0.0] 
+    μ2[:,3] = [0.0, 0.0, 1.0, 1.0]
+
+
+    nt = 1000
+    label = fill(0,nt)
+    X1 = fill(0.0, d ,nt)
+    X2 = fill(0.0, d ,nt)
+    for i in 1:nt
+        l = rand(1:3)
+        label[i] = l
+        X1[:,i] .= μ1[:,l] .+ 0.125*randn(d)
+        X2[:,i] .= μ2[:,l] .+ 0.125*randn(d)
+    end
+    mstats1 = MultivariateStats.multiclass_lda_stats(3, X1, label)
+    mstats2 = MultivariateStats.multiclass_lda_stats(3, X2, label)
+    # concatenate the matrices
+    Sw = fill(0.0, 2d,2d)
+    Sb = fill(0.0, 2d,2d)
+
+    Sw[1:d, 1:d] = mstats1.Sw
+    Sb[1:d, 1:d] = mstats1.Sb
+
+    Sw[d+1:2d, d+1:2d] = mstats2.Sw
+    Sb[d+1:2d, d+1:2d] = mstats2.Sb
+
+    u1,s1,v1 = svd(mstats1.Sb)
+    u2,s2,v2 = svd(mstats2.Sb)
+    
+    # verify that the two between-scatter matrices are close to orthogonal
+    #@show norm(u1[:,1:2]'*u2[:,1:2])
+
+    w, f0, f1 = OrthogonalLDA.orthogonal_lda(Sb, Sw, 2;debug=missing)
+    Y1 = w[1:d, :]'*X1
+    Y2 = w[d+1:2d, :]'*X2
+    μ1p = fill(0.0, 2,3)
+    μ1p[:,1] = mean(Y1[:,label.==1],dims=2)
+    μ1p[:,2] = mean(Y1[:,label.==2],dims=2)
+    μ1p[:,3] = mean(Y1[:,label.==3],dims=2)
+
+    μ2p = fill(0.0, 2,3)
+    μ2p[:,1] = mean(Y2[:,label.==1],dims=2)
+    μ2p[:,2] = mean(Y2[:,label.==2],dims=2)
+    μ2p[:,3] = mean(Y2[:,label.==3],dims=2)
+
+    #decode 
+    p1 = 0.0
+    p2 = 0.0
+    for i in 1:nt
+        dd = dropdims(sum(abs2, Y1[:,i] .- μ1p,dims=1),dims=1)
+        p1 += (argmin(dd) == label[i])
+        dd = dropdims(sum(abs2, Y2[:,i] .- μ2p,dims=1),dims=1)
+        p2 += (argmin(dd) == label[i])
+    end
+    p1 /= nt
+    p2 /= nt
+    # make sure we can decode the responses
+    @test p1 > 0.9
+    @test p2 > 0.9
+
+  #μ1pp = w[1:d,:]*μ1p
+  #μ2pp = w[d+1:2d,:]*μ2p
+
+#    w = OrthogonalLDA.orthogonal_lda(mstats1.Sb, mstats2.Sb, mstats1.Sw, mstats2.Sw, 2)
+#    Y1 = w[1:d, :]'*X1
+#    Y2 = w[d+1:2d, :]'*X2
+#    μ1p[:,1] = mean(Y1[:,label.==1],dims=2)
+#    μ1p[:,2] = mean(Y1[:,label.==2],dims=2)
+#    μ1p[:,3] = mean(Y1[:,label.==3],dims=2)
+#
+#    μ2p[:,1] = mean(Y2[:,label.==1],dims=2)
+#    μ2p[:,2] = mean(Y2[:,label.==2],dims=2)
+#    μ2p[:,3] = mean(Y2[:,label.==3],dims=2)
+#    μ1pp = w[1:d,:]*μ1p
+#    μ2pp = w[d+1:2d,:]*μ2p
+#    @show μ1pp, μ1
+#    @show μ2pp, μ2
+end
