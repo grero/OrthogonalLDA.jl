6c39e7e96d07b939bef245ff5672b6b5fd772d7c
Roger Herikstad <roger.herikstad@gmail.com>
2022-07-07 14:30:41 +0800

    Adds function for arbitrary number of joint spaces

--- src/OrthogonalLDA.jl
+++ src/OrthogonalLDA.jl
@@ -117,4 +117,57 @@ function orthogonal_lda(Sb1::AbstractMatrix{Float64}, Sb2::AbstractMatrix{Float6
         debug=debug,
     )
 end
+
+function orthogonal_lda(Sb::Vector{T}, Sw::Vector{T}, r::Vector{Int64};debug=:default, method=:quasi_newton) where T <: AbstractMatrix{Float64}
+	d = size(first(Sb),1)
+	n = length(Sb)
+	for sb in Sb
+		if size(sb,1) != d
+			error("All dimensions must be equal")
+		end
+	end
+
+	if !ismissing(debug) && debug == :default
+		debug=[:Iteration, " ", :Cost, "\n", 1, :Stop]
+	end
+
+	function F(::Stiefel, W::Array{Float64,2})
+		a = 0.0
+		offset = 0
+		for (_Sb, _Sw,_r) in zip(Sb,Sw,r)
+			_W = W[:,(offset+1):(offset+_r)]
+			a += -tr(_W'*_Sb*_W)/tr(_W'*_Sw*_W)
+			offset += _r
+		end
+		a
+    end
+
+	function gradF(M, W::Array{Float64,2})
+		G = fill(0.0, d, sum(r))
+		offset = 0
+		for (_Sb, _Sw,_r) in zip(Sb, Sw,r)
+			_W = W[:,(offset+1):(offset+_r)]
+			trb = tr(_W'*_Sb*_W)
+			trw = tr(_W'*_Sw*_W)
+			G[:,(offset+1):(offset+_r)] = -((_Sb*_W + _Sb'*_W).*trw - trb.*(_Sw*_W + _Sw'*_W))./trw^2
+			offset += _r
+		end
+        return project(M, W, G)
+	end
+
+	M = Stiefel(d, sum(r))
+	w = random_point(M)
+    w = quasi_Newton(
+        M,
+        F,
+        gradF,
+        w;
+        memory_size=32,
+        #evaluation=MutatingEvaluation(),
+        cautious_update=true,
+        vector_transport_method=ProjectionTransport(),
+        stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-9)),
+        debug=debug,
+    )
+end
 end # module
