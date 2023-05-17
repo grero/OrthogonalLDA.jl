ba67788e5467deef96c07365b068e4d5d6e7b7a3
Roger Herikstad <roger.herikstad@gmail.com>
2023-05-17 14:00:05 +0800

    Generalizes to many spaces and adds PCA

--- src/OrthogonalLDA.jl
+++ src/OrthogonalLDA.jl
@@ -4,6 +4,7 @@ using Manifolds
 using Manopt
 using ManifoldsBase
 using LinearAlgebra
+using Random
 
 import Manifolds: _vector_transport_to
 
@@ -12,113 +13,7 @@ end
 
 Manifolds._vector_transport_to(M::Stiefel, Y, p, X, q, T::IdentityTransport) = (Y .= project(M, q, X))
 
-function orthogonal_lda(Sb::AbstractMatrix{Float64}, Sw::AbstractMatrix{Float64}, r::Int64;debug::Union{Missing, Symbol}=:default, method=:quasi_newton)
-    if !ismissing(debug) && debug == :default
-		debug=[:Iteration, " ", :Cost, "\n", 1, :Stop]
-	end
-	d = size(Sb,1)
-	d == size(Sb,2) == size(Sw,1) == size(Sw,2) || error("Matrices must be of equal size")
-	M = Stiefel(d, r)
-	
-	function F(::Stiefel, W::Array{Float64,2})
-		-tr(W'*Sb*W)/tr(W'*Sw*W)
-	end
-	
-	#G = fill(0.0, d, r)
-	function gradF(M, W::Array{Float64,2})
-		trb = tr(W'*Sb*W)
-		trw = tr(W'*Sw*W)
-		G = -((Sb*W + Sb'*W).*trw - trb.*(Sw*W + Sw'*W))./trw^2
-		return project(M, W, G)
-	end
-	w = rand(M)
-	f0 = F(M, w)
-	if method == :quasi_newton
-		w = quasi_Newton(
-		    M,
-		    F,
-		    gradF,
-		    w;
-		    memory_size=32,
-			#evaluation=MutatingEvaluation(),
-		    cautious_update=true,
-            vector_transport_method=ProjectionTransport(),
-		    stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-9)),
-		    debug=debug,
-		)
-	elseif method == :gradient_descent
-		w = gradient_descent(
-		    M,
-		    F,
-		    gradF,
-		    w;
-		    stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-8)),
-		    debug=debug,
-		)
-	elseif method == :trust_regions
-		w = trust_regions(M,
-						  F,
-						  gradF,
-						  ApproxHessianFiniteDifference(M, w, gradF),
-						  w;
-						#vector_transport_method=IdentityTransport(),
-		    			#stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-8)),
-		    			debug=debug
-		)
-	else
-		error("Unknown method $(method)")
-	end
-	f1 = F(M, w)
-	w, f0, f1
-end
-
-function orthogonal_lda(Sb1::AbstractMatrix{Float64}, Sb2::AbstractMatrix{Float64},Sw1::AbstractMatrix{Float64}, Sw2::AbstractMatrix{Float64}, r::Int64;debug=:default, method=:quasi_newton)
-    d1 = size(Sb1,1)
-    d2 = size(Sb2,1)
-	d1 == d2 || error("All dimensions must be equal")
-	d = d1
-
-	if !ismissing(debug) && debug == :default
-		debug=[:Iteration, " ", :Cost, "\n", 1, :Stop]
-	end
-
-	function F(::Stiefel, W::Array{Float64,2})
-        W1 = W[:,1:r]
-        W2 = W[:,r+1:2r]
-        a = -tr(W1'*Sb1*W1)/tr(W1'*Sw1*W1)
-        b = -tr(W2'*Sb2*W2)/tr(W2'*Sw2*W2)
-        a + b
-    end
-
-	function gradF(M, W::Array{Float64,2})
-        W1 = W[:,1:r]
-        W2 = W[:,r+1:2r]
-		trb1 = tr(W1'*Sb1*W1)
-		trb2 = tr(W2'*Sb2*W2)
-		trw1 = tr(W1'*Sw1*W1)
-		trw2 = tr(W2'*Sw2*W2)
-		G1 = -((Sb1*W1 + Sb1'*W1).*trw1 - trb1.*(Sw1*W1 + Sw1'*W1))./trw1^2
-		G2 = -((Sb2*W2 + Sb2'*W2).*trw2 - trb2.*(Sw2*W2 + Sw2'*W2))./trw2^2
-        return project(M, W, cat(G1,G2,dims=2))
-	end
-
-	M = Stiefel(d, 2r)
-	w = rand(M)
-    w = quasi_Newton(
-        M,
-        F,
-        gradF,
-        w;
-        memory_size=32,
-        #evaluation=MutatingEvaluation(),
-        cautious_update=true,
-        vector_transport_method=ProjectionTransport(),
-        stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-9)),
-        debug=debug,
-    )
-end
-
-function orthogonal_lda(Sb::Vector{T}, Sw::Vector{T}, r::Vector{Int64};debug=:default, method=:quasi_newton) where T <: AbstractMatrix{Float64}
+function orthogonal_lda(Sb::Vector{T}, Sw::Vector{T}, r::Vector{Int64};debug=:default, method=:quasi_newton, RNG=Random.GlOBAL_RNG) where T <: AbstractMatrix{Float64}
 	d = size(first(Sb),1)
 	n = length(Sb)
 	for sb in Sb
@@ -156,7 +51,7 @@ function orthogonal_lda(Sb::Vector{T}, Sw::Vector{T}, r::Vector{Int64};debug=:de
 	end
 
 	M = Stiefel(d, sum(r))
-	w = random_point(M)
+	w = rand(RNG, M)
     w = quasi_Newton(
         M,
         F,
@@ -167,6 +62,61 @@ function orthogonal_lda(Sb::Vector{T}, Sw::Vector{T}, r::Vector{Int64};debug=:de
         cautious_update=true,
         vector_transport_method=ProjectionTransport(),
         stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-9)),
+        debug=debug,
+    )
+end
+
+function orthogonal_pca(Σ::Vector{T}, r::Vector{Int64};debug=:default, method=:quasi_newton, RNG=Random.GLOBAL_RNG) where T <: AbstractMatrix{Float64}
+	d = size(first(Σ),1)
+	n = length(Σ)
+	for S in Σ
+		if size(S,1) != d
+			error("All dimensions must be equal")
+		end
+	end
+	S = fill(0.0, n)
+	for (i,_Σ) in enumerate(Σ)
+		u,s,v = svd(_Σ)
+		S[i] = sum(s)
+	end
+	if !ismissing(debug) && debug == :default
+		debug=[:Iteration, " ", :Cost, "\n", 1, :Stop]
+	end
+
+	function F(::Stiefel, W::Array{Float64,2})
+		a = 0.0
+		offset = 0
+		for (_Σ, _r, _s) in zip(Σ,r,S)
+			_W = W[:,(offset+1):(offset+_r)]
+			a += tr(_W'*_Σ*_W)/_s
+			offset += _r
+		end
+		-a/n
+    end
+
+	function gradF(M, W::Array{Float64,2})
+		G = fill(0.0, d, sum(r))
+		offset = 0
+		for (_Σ, _s,_r) in zip(Σ,S, r)
+			_W = W[:,(offset+1):(offset+_r)]
+			G[:,(offset+1):(offset+_r)] = -(_Σ*_W + _Σ'*_W)./(_s*n)
+			offset += _r
+		end
+        return project(M, W, G)
+	end
+
+	M = Stiefel(d, sum(r))
+	w = rand(RNG, M)
+    w = quasi_Newton(
+        M,
+        F,
+        gradF,
+        w;
+        memory_size=32,
+        #evaluation=MutatingEvaluation(),
+        cautious_update=true,
+        vector_transport_method=ProjectionTransport(),
+        stopping_criterion=StopWhenGradientNormLess(norm(M, w, gradF(M, w)) * 10^(-14)),
         debug=debug,
     )
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,41 @@ using OrthogonalLDA
 using LinearAlgebra
 using MultivariateStats
 using Statistics
+using StableRNGs
 
 using Test
 
+@testset "PCA" begin
+	rng = StableRNG(1234)
+	q,r = qr(randn(rng, 4,4))
+	W1 = q[:,1:2]
+	W2 = q[:, 3:4]
+	
+	r1 = randn(rng, 2,2)
+	Σ1 = W1*r1'*r1*W1' .+ 0.0*randn(rng, 4,4)
 
-@testset "Basic" begin
+	r2 = randn(rng, 2,2)
+	Σ2 = W2*r2'*r2*W2' + 0.0*randn(rng, 4,4)
+
+	W = OrthogonalLDA.orthogonal_pca([Σ1, Σ2], [2,2];debug=missing,RNG=rng)
+	W1p = W[:,1:2]
+	W2p = W[:, 3:4]
+	# test that the solutions overlap with the ground truth
+	@show  norm(W1p - W1)
+	u,s,v = svd(W1p'*W1)
+    @test all(abs.(s .- 1.0) .< sqrt(eps(Float64)))
+	u,s,v = svd(W2p'*W2)
+    @test all(abs.(s .- 1.0) .< sqrt(eps(Float64)))
+
+	# test that the solutions do not overlap across 
+	u,s,v = svd(W2p'*W1)
+    @test all(s .< sqrt(eps(Float64)))
+	u,s,v = svd(W1p'*W2)
+    @test all(s .< sqrt(eps(Float64)))
+end
+
+@testset "LDA" begin
+	rng = StableRNG(1234)
     d = 4
 	nc = 3
 	r = nc-1
@@ -15,11 +45,11 @@ using Test
     μ1[:,1] = [0.0,1.0, 0.0, 0.0]
     μ1[:,2] = [1.0, 0.0, 0.0, 0.0]
     μ1[:,3] = [1.0, 1.0, 0.0, 0.0]
-    
+
     # 2nd plane
     μ2  = fill(0.0, d, 3)
     μ2[:,1] = [0.0, 0.0, 0.0, 1.0]
-    μ2[:,2] = [0.0, 0.0, 1.0, 0.0] 
+    μ2[:,2] = [0.0, 0.0, 1.0, 0.0]
     μ2[:,3] = [0.0, 0.0, 1.0, 1.0]
 
 
@@ -28,13 +58,13 @@ using Test
     X1 = fill(0.0, d ,nt)
     X2 = fill(0.0, d ,nt)
     for i in 1:nt
-        l = rand(1:3)
+        l = rand(rng, 1:3)
         label[i] = l
-        X1[:,i] .= μ1[:,l] .+ 0.125*randn(d)
-        X2[:,i] .= μ2[:,l] .+ 0.125*randn(d)
+        X1[:,i] .= μ1[:,l] .+ 0.125*randn(rng, d)
+        X2[:,i] .= μ2[:,l] .+ 0.125*randn(rng, d)
     end
-    mstats1 = MultivariateStats.multiclass_lda_stats(X1, label)
-    mstats2 = MultivariateStats.multiclass_lda_stats(X2, label)
+    mstats1 = MultivariateStats.multiclass_lda_stats(nc, X1, label)
+    mstats2 = MultivariateStats.multiclass_lda_stats(nc, X2, label)
     # concatenate the matrices
     Sw = fill(0.0, 2d,2d)
     Sb = fill(0.0, 2d,2d)
@@ -47,12 +77,12 @@ using Test
 
     u1,s1,v1 = svd(mstats1.Sb)
     u2,s2,v2 = svd(mstats2.Sb)
-    
-	wr = OrthogonalLDA.orthogonal_lda([mstats1.Sb, mstats2.Sb], [mstats1.Sw, mstats2.Sw], [r,r];debug=missing)
+
+	wr = OrthogonalLDA.orthogonal_lda([mstats1.Sb, mstats2.Sb], [mstats1.Sw, mstats2.Sw], [r,r];debug=missing, RNG=rng)
 	w1 = wr[:, 1:r]
 	w2 = wr[:,r+1:2r]
 	nn = norm(w1'*w2)
-	@test nn < 1e-14
+    @test nn < sqrt(eps(Float64))
     Y1 = w1'*X1
     Y2 = w2'*X2
     μ1p = fill(0.0, 2,3)
@@ -65,7 +95,7 @@ using Test
     μ2p[:,2] = mean(Y2[:,label.==2],dims=2)
     μ2p[:,3] = mean(Y2[:,label.==3],dims=2)
 
-    #decode 
+    #decode
     p1 = 0.0
     p2 = 0.0
     for i in 1:nt
@@ -80,22 +110,4 @@ using Test
     @test p1 > 0.9
     @test p2 > 0.9
 
-    @time w, f0, f1 = OrthogonalLDA.orthogonal_lda(Sb, Sw, 2;debug=missing)
-  #μ1pp = w[1:d,:]*μ1p
-  #μ2pp = w[d+1:2d,:]*μ2p
-
-#    w = OrthogonalLDA.orthogonal_lda(mstats1.Sb, mstats2.Sb, mstats1.Sw, mstats2.Sw, 2)
-#    Y1 = w[1:d, :]'*X1
-#    Y2 = w[d+1:2d, :]'*X2
-#    μ1p[:,1] = mean(Y1[:,label.==1],dims=2)
-#    μ1p[:,2] = mean(Y1[:,label.==2],dims=2)
-#    μ1p[:,3] = mean(Y1[:,label.==3],dims=2)
-#
-#    μ2p[:,1] = mean(Y2[:,label.==1],dims=2)
-#    μ2p[:,2] = mean(Y2[:,label.==2],dims=2)
-#    μ2p[:,3] = mean(Y2[:,label.==3],dims=2)
-#    μ1pp = w[1:d,:]*μ1p
-#    μ2pp = w[d+1:2d,:]*μ2p
-#    @show μ1pp, μ1
-#    @show μ2pp, μ2
 end
